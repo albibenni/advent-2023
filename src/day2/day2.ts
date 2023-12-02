0852d3ecb3757c1de4ce6882ff5d79bf21db5ec1
Alberto Benatti <albi.benni8@gmail.com>
2023-12-02 11:30:27 +0100

    day 1 test working

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { readFileSync } from "fs";
 
 const input = readFileSync("./src/day2/input/test.txt", "utf8").split("\n");
@@ -17,58 +18,57 @@ const getStringDigit = (str: string): string => {
 };
 
 const getGameNumber = (str: string): number => {
-  let i = 4;
-  let currChar = str.charAt(i++);
+  let i = 5;
+  let currChar = str.charAt(i);
   let gameNumber = 0;
   while (!isNaN(parseInt(currChar))) {
     if (gameNumber !== 0) {
       gameNumber = parseInt(gameNumber + "" + currChar);
     }
     gameNumber = parseInt(currChar);
+    currChar = str.charAt(++i);
   }
   return gameNumber;
 };
 
-const matchMap = (
-  str: string,
-  map: Map<string, number>,
-): Map<string, number> => {
+const matchMap = (str: string): Map<string, number> => {
   let myKey = "";
   let num = 0;
-  //   console.log(map.keys());
-
+  const map: Map<string, number> = new Map();
   for (let key of extractedCubes.keys()) {
     if (str.includes(key)) {
       myKey = key;
       num = parseInt(getStringDigit(str));
+      map.set(myKey, num);
     }
-    map.set(myKey, num);
   }
   return map;
 };
 
 input.forEach((line) => {
   const gameNumber = getGameNumber(line);
-  let currentCubes: Map<string, number> = new Map();
+  const currentCubes: Map<string, number> = new Map();
+  currentCubes.set("red", 0);
+  currentCubes.set("blue", 0);
+  currentCubes.set("green", 0);
   let i = 6 + gameNumber.toString.length;
   let start = i;
   for (i; i < line.length - 2; i++) {
     if (line.charAt(i) === "," || line.charAt(i) === ";") {
-      const str = line.substring(start, i);
+      const str = line.substring(start, i).trim();
       start = i + 1;
-      currentCubes = matchMap(str, currentCubes);
+      const mapTemp = matchMap(str);
+      const [firstKey] = mapTemp.keys();
+      const [firstValue] = mapTemp.values();
+      currentCubes.set(firstKey!, firstValue! + currentCubes.get(firstKey!)!);
     }
   }
-  for (let i = 0; i < currentCubes.size; i++) {
-    if (currentCubes.get("red")! !== extractedCubes.get("red")!) {
-      break;
-    }
-    if (currentCubes.get("blue")! !== extractedCubes.get("blue")!) {
-      break;
-    }
-    if (currentCubes.get("green")! !== extractedCubes.get("green")!) {
-      break;
-    }
+
+  if (
+    currentCubes.get("red")! <= extractedCubes.get("red")! &&
+    currentCubes.get("blue")! <= extractedCubes.get("blue")! &&
+    currentCubes.get("green")! <= extractedCubes.get("green")!
+  ) {
     possibleGames.push(gameNumber);
   }
 });

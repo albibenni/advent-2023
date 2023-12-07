c495ffea3501ae4dd044c457b76caef53b68b661
Alberto Benatti <albi.benni8@gmail.com>
2023-12-07 11:55:23 +0100

    wip

--- src/day7/day7.ts
+++ src/day7/day7.ts
@@ -56,4 +56,30 @@ const part1 = (rawInput: string) => {
   return types;
 };
 
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const types = input
+    .map(([cards, bid]) => {
+      const s = [...new Set(cards!.split(""))]
+        .map((c) =>
+          c === "J" ? 0 : cards!.split("").filter((x) => x === c).length,
+        )
+        .sort((a, b) => b - a)
+        .map((c, i) => (i === 0 ? c + (cards!.match(/J/g)?.length ?? 0) : c))
+        .join("")
+        .padEnd(5, "0");
+      const p = cards!
+        .split("")
+        .map((c) => letters[strengthWithJoker.indexOf(c)])
+        .join("");
+      return { s, c: cards, b: Number(bid), p };
+    })
+    .sort((a, b) => b.p.localeCompare(a.p))
+    .sort((a, b) => a.s.localeCompare(b.s))
+    .map((x, i) => ({ ...x, r: i + 1 }));
+  // console.log([...types].reverse());
+  return types.reduce((acc, v) => acc + v.b * v.r, 0);
+};
+
 log(part1(readFileSync("./src/day7/input/input.txt", "utf8") as string));
+log(part2(readFileSync("./src/day7/input/input.txt", "utf8") as string));

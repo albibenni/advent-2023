7a39a12c99b8de3949731ea5177945ed93a92858
Alberto Benatti <albi.benni8@gmail.com>
2023-12-13 09:06:10 +0100

    p1

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -1,3 +1,134 @@
 /**
  * --- Day 13: Point of Incidence ---
  */
+
+// type Rock = "#";
+// type Ash = ".";
+
+// const isRock = (str: string): str is Rock => str === "#";
+// const isAsh = (str: string): str is Ash => str === ".";
+
+// let verticalReflection = true;
+// let horizontalReflection = true;
+
+// const isReflecting = (matrix: string[][]): void => {
+//   const matrixLength = matrix.length;
+//   const matrixWidth = matrix[0]!.length;
+// };
+
+import { arraysEqual } from "../utils.js";
+import {
+  Grid as AbstractGrid,
+  column,
+  maxX,
+  maxY,
+  parseGrid,
+  renderGrid,
+} from "../grid-utils.js";
+import { readFileSync } from "fs";
+
+export type Tile = "." | "#";
+export type Grid = AbstractGrid<Tile>;
+
+export type Mirror = {
+  position: number;
+  orientation: "H" | "V";
+};
+
+export function transpose(g: Grid): Grid {
+  //@ts-ignore
+  return g[0].map((_, i) => column(g, i));
+}
+
+function findMirror(pattern: Grid): Mirror {
+  //const horiz = search(pattern);
+  //const vert = search(transpose(pattern)).map(m=>({...m, orientation: "V"} as Mirror));
+
+  return (
+    search(pattern) ??
+    ((m) => ({ ...m, orientation: "V" }))(search(transpose(pattern))!)
+  );
+
+  function search(pattern: Grid): Mirror | null {
+    console.log("findMirror, searching", "\n" + renderGrid(pattern));
+
+    const mY = maxY(pattern);
+    //const mX = maxX(pattern)
+
+    for (let y = 1; y < mY; ++y) {
+      const cur = pattern[y];
+      const last = pattern[y - 1];
+
+      if (arraysEqual(cur!, last!)) {
+        let valid = true;
+        const range = Math.min(y, mY - y);
+        console.log("findMirror; checking y", y, "range", range);
+
+        for (let delta = 0; delta < range; ++delta) {
+          const next = pattern[y + delta];
+          const prev = pattern[y - 1 - delta];
+
+          console.log(
+            "findMirror; checking delta",
+            delta,
+            "\n+1",
+            next!.join(""),
+            "\n-1",
+            prev!.join(""),
+          );
+
+          if (!arraysEqual(next!, prev!)) {
+            valid = false;
+            break;
+          }
+        }
+
+        if (valid)
+          return {
+            orientation: "H",
+            position: y,
+          };
+      }
+    }
+    return null;
+  }
+}
+
+export async function main(lines: string[]) {
+  const cleanedLines = lines.map((l) => l.trim());
+
+  const patterns = cleanedLines
+    .reduce(
+      (acc, cur) => {
+        const isBlank = cur == "";
+
+        if (isBlank) acc.push([]);
+        else acc.at(-1)!.push(cur);
+
+        return acc;
+      },
+      [[]] as string[][],
+    )
+    .filter((p) => p.length !== 0);
+
+  console.log(patterns);
+
+  const mirrors = patterns.map((p) => findMirror(parseGrid(p)));
+
+  console.log(mirrors);
+
+  const answer = mirrors
+    .flat()
+    .reduce(
+      (acc, { orientation, position }) =>
+        acc + position * (orientation == "H" ? 100 : 1),
+      0,
+    );
+
+  console.log(answer);
+}
+
+// if (import.meta.main) await runMain(main);
+const input = readFileSync("./src/day13/input/input.txt", "utf8");
+
+main(input.split("\n"));
---
+++ src/utils.ts
@@ -0,0 +1,52 @@
+// export type Main = (inputLines: string[]) => Promise<void>;
+// export async function runMain(main: Main) {
+//   const [_, mode = "e"] = Deno.args;
+
+//   const fileToLoad = mode == "m" ? "input.txt" : "example.txt";
+//   const inputLines = (await Deno.readTextFile(fileToLoad)).split(/\r|\n|\r\n/);
+
+//   await main(inputLines);
+// }
+
+export function sum(nums: number[], mapper?: (v: number) => number): number;
+export function sum<T>(nums: T[], mapper: (v: T) => number): number;
+
+export function sum<T>(nums: T[], mapper?: (v: T) => number): number {
+  const reducer: (acc: number, curr: T) => number =
+    typeof mapper === "function"
+      ? (acc, curr) => acc + mapper(curr)
+      : (acc, curr) => acc + +curr;
+
+  return nums.reduce(reducer, 0);
+}
+
+export function product(nums: number[], mapper?: (v: number) => number): number;
+export function product<T>(nums: T[], mapper: (v: T) => number): number;
+
+export function product<T>(nums: T[], mapper?: (v: T) => number): number {
+  const reducer: (acc: number, curr: T) => number =
+    typeof mapper === "function"
+      ? (acc, curr) => acc * mapper(curr)
+      : (acc, curr) => acc * +curr;
+
+  return nums.reduce(reducer, 1);
+}
+
+export function setIntersection<T>(a: Set<T>, b: Set<T>): Set<T> {
+  const [larger, smaller] = a.size > b.size ? [a, b] : [b, a];
+
+  return new Set(_intersect(larger, smaller));
+}
+function* _intersect<T>(larger: Set<T>, smaller: Set<T>) {
+  for (const item of smaller) if (larger.has(item)) yield item;
+}
+
+export function arraysEqual<T>(l: T[], r: T[]): boolean {
+  return l.length == r.length && l.every((e, i) => e === r[i]);
+}
+
+// these 3 stolen from https://stackoverflow.com/a/61352020/
+export const gcd = (a: number, b: number): number =>
+  b == 0 ? a : gcd(b, a % b);
+export const lcm = (a: number, b: number) => (a / gcd(a, b)) * b;
+export const lcmAll = (ns: number[]) => ns.reduce(lcm, 1);

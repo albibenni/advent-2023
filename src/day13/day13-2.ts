0d3ae2e6fef0c5294d25823f2916ed2da4e4c868
Alberto Benatti <albi.benni8@gmail.com>
2023-12-13 09:08:58 +0100

    p2

--- src/day13/day13-2.ts
+++ src/day13/day13-2.ts
@@ -1,21 +1,3 @@
-/**
- * --- Day 13: Point of Incidence ---
- */
-
-// type Rock = "#";
-// type Ash = ".";
-
-// const isRock = (str: string): str is Rock => str === "#";
-// const isAsh = (str: string): str is Ash => str === ".";
-
-// let verticalReflection = true;
-// let horizontalReflection = true;
-
-// const isReflecting = (matrix: string[][]): void => {
-//   const matrixLength = matrix.length;
-//   const matrixWidth = matrix[0]!.length;
-// };
-
 import { arraysEqual } from "../utils.js";
 import {
   Grid as AbstractGrid,
@@ -26,45 +8,45 @@ import {
   renderGrid,
 } from "../grid-utils.js";
 import { readFileSync } from "fs";
+import { Tile, Grid, Mirror, transpose } from "./day13.js";
 
-export type Tile = "." | "#";
-export type Grid = AbstractGrid<Tile>;
-
-export type Mirror = {
-  position: number;
-  orientation: "H" | "V";
-};
-
-export function transpose(g: Grid): Grid {
-  //@ts-ignore
-  return g[0].map((_, i) => column(g, i));
+//@ts-ignore
+function arrayEqualErrors<T>(l: T[], r: T[]): number {
+  return l.length == r.length
+    ? l.reduce((acc, curL, i) => acc + (curL === r[i] ? 0 : 1), 0)
+    : Math.max(l.length, r.length);
 }
 
-function findMirror(pattern: Grid): Mirror {
-  //const horiz = search(pattern);
-  //const vert = search(transpose(pattern)).map(m=>({...m, orientation: "V"} as Mirror));
+type SmudgedMirror = Mirror & { errors: number };
 
-  return (
-    search(pattern) ??
-    ((m) => ({ ...m, orientation: "V" }))(search(transpose(pattern))!)
-  );
+function findMirror(pattern: Grid): SmudgedMirror[] {
+  const horiz = search(pattern);
+  const vert = search(transpose(pattern)).map((m) => ({
+    ...m,
+    orientation: "V" as const,
+  }));
 
-  function search(pattern: Grid): Mirror | null {
+  return [...horiz, ...vert];
+
+  function search(pattern: Grid, maxErrors = 1) {
     console.log("findMirror, searching", "\n" + renderGrid(pattern));
+    const mirrors: SmudgedMirror[] = [];
 
     const mY = maxY(pattern);
-    //const mX = maxX(pattern)
 
     for (let y = 1; y < mY; ++y) {
       const cur = pattern[y];
       const last = pattern[y - 1];
 
-      if (arraysEqual(cur!, last!)) {
-        let valid = true;
+      const initErrors = arrayEqualErrors(cur!, last!);
+
+      if (initErrors <= maxErrors) {
+        let errors = initErrors;
+
         const range = Math.min(y, mY - y);
         console.log("findMirror; checking y", y, "range", range);
 
-        for (let delta = 0; delta < range; ++delta) {
+        for (let delta = 1; delta < range; ++delta) {
           const next = pattern[y + delta];
           const prev = pattern[y - 1 - delta];
 
@@ -77,20 +59,21 @@ function findMirror(pattern: Grid): Mirror {
             prev!.join(""),
           );
 
-          if (!arraysEqual(next!, prev!)) {
-            valid = false;
-            break;
-          }
+          errors += arrayEqualErrors(next!, prev!);
+
+          if (errors > maxErrors) break;
         }
 
-        if (valid)
-          return {
+        console.log("total errors", errors);
+        if (errors <= maxErrors)
+          mirrors.push({
             orientation: "H",
             position: y,
-          };
+            errors,
+          });
       }
     }
-    return null;
+    return mirrors;
   }
 }
 
@@ -113,12 +96,21 @@ export async function main(lines: string[]) {
 
   console.log(patterns);
 
+  /*
+    for(const p of patterns.map((p)=>parseGrid<Tile>(p))) {
+        const tr = transpose(p);
+        console.log("P\n"+renderGrid(p)+"\n v v v\n"+renderGrid(tr));
+    }
+    return;
+    */
+
   const mirrors = patterns.map((p) => findMirror(parseGrid(p)));
 
   console.log(mirrors);
 
   const answer = mirrors
     .flat()
+    .filter((m) => m.errors === 1)
     .reduce(
       (acc, { orientation, position }) =>
         acc + position * (orientation == "H" ? 100 : 1),
